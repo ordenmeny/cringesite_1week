ffe1a2c97986217e7c715f4db9c8cd550fbe5f32
Dmitriy Alexandrov <newsmobileads@gmail.com>
2024-12-07 17:46:45 +0500

    Улучшен код

--- api_app/urls.py
+++ api_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('create-joke/', JokeCreateAPIView.as_view()),
-    # path('detail/<int:pk>/', JokeDetailAPIView.as_view()),
+    path('create-joke/', JokeCreateAPIView.as_view()),  # POST
+    path('list-jokes/', ListJokes.as_view()),  # GET
+    path('retrieve/<int:pk>/', JokeRetrieveAPIView.as_view()),  # GET, DELETE
 ]
